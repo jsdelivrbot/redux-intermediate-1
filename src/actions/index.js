e9a40f9aaaa8c5a8bd401b470f836018bea1a299
Thomas Choi <thomastaechoi@gmail.com>
2017-04-13 15:51:38 -0700

    ajax requests with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const WEATHER_API_KEY = "9f7717fcdf62f2d7d1624b028881ee0b";
+const WEATHER_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+function getWeather (city) {
+  const url = `${WEATHER_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
+
+export default getWeather
