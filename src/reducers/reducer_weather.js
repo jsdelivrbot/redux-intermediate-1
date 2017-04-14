7c96ca07824e1e508d5d4469a94355960fc9eaf5
Thomas Choi <thomastaechoi@gmail.com>
2017-04-14 16:30:51 -0700

    building list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,8 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function (state = [], action) {
   if (action.type) {
     if (action.type == FETCH_WEATHER) {
-      console.log('action received')
-      return [ action.type.payload, ...state ];
+      return [ action.payload.data, ...state ];
     }
   }
 
